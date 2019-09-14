824d389b42a62e019bb28631c08bdc1a12e23dcf
Ashish Ranjan <ashish72@pm.me>
2019-09-15 00:29:24 +0530

    feat(cmus): Install on Nix

--- src/os/install/nix/misc-tools.sh
+++ src/os/install/nix/misc-tools.sh
@@ -12,6 +12,7 @@ print_in_purple "\n   Miscellaneous Tools\n\n"
 
 nix_install "assh" "nixpkgs.assh"
 nix_install "bat" "nixpkgs.bat"
+nix_install "cmus" "nixpkgs.cmus"
 nix_install "hub" "nixpkgs.gitAndTools.hub"
 nix_install "Icdiff" "nixpkgs.icdiff"
 nix_install "imgcat" "nixpkgs.imgcat"
