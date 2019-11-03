8f66cd3791a6b35808ef226c9e3b2ce2b8c9df31
Ashish Ranjan <ashish@tug.ro>
2019-11-03 18:45:10 +0000

    fix(setup): Use HTTPS link as origin URL

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -2,7 +2,7 @@
 
 declare -r GITHUB_REPOSITORY="musq/dotfiles"
 
-declare -r DOTFILES_ORIGIN="git@github.com:$GITHUB_REPOSITORY.git"
+declare -r DOTFILES_ORIGIN="https://github.com/$GITHUB_REPOSITORY.git"
 declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/master"
 declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/master/src/os/utils.sh"
 
