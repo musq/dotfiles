719d56b8da8f044fa01cc6a8abda13d7bcd84c95
Ashish Ranjan <ashish@tug.ro>
2019-11-03 15:16:23 +0000

    fix(ssh): Fix ssh-agent forwarding
    If the current machine is a desktop, tell ssh-agent to piggyback on the
    gpg-agent for authentication. This means setting SSH_AUTH_SOCK to
    S.gpg-agent.ssh. Whereas, during ssh-agent forwarding, the server
    machine should use the default socket provided by the sshd daemon.
    
    Hence, the condition to check if the current system is a desktop or a
    server has been added. Now the SSH_AUTH_SOCK is set to use the gpg-agent
    on S.gpg-agent.ssh only when there is a keygrip specified in the
    ~/.gnupg/sshcontrol file.

--- src/shell/profile.d/gpg.sh
+++ src/shell/profile.d/gpg.sh
@@ -6,8 +6,19 @@ export GPG_TTY=$(tty)
 
 unset SSH_AGENT_PID
 
-if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+# A Desktop contains private keys
+# A Server does not contain private keys. It uses ssh-agent/gpg-agent to
+# borrow keys for a temporary duration
+#
+# Set SSH_AUTH_SOCK only if sshcontrol file contains a keygrip
+# It indicates that the current system is a desktop and not a server
+
+if [ -e "$HOME/.gnupg/sshcontrol" ] \
+    && egrep -q "^[ ]*[0-9A-Z]{40}" "$HOME/.gnupg/sshcontrol" \
+    && [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+
     export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+
 fi
 
 gpg-connect-agent updatestartuptty /bye >/dev/null
