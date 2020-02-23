458f8c0fecfb4ff1c574ce81ef0b19e4b95f65bd
Ashish Ranjan <ashish@tug.ro>
2020-02-23 16:04:48 +0530

    Revert "feat(buku): Install on Nix"
    This reverts commit dedefb8896a2ed3482e66323627fb11385520830.

--- src/os/install/nix/misc-tools.sh
+++ src/os/install/nix/misc-tools.sh
@@ -12,7 +12,6 @@ print_in_purple "\n   Miscellaneous Tools\n\n"
 
 nix_install "assh" "nixpkgs.assh"
 nix_install "bat" "nixpkgs.bat"
-nix_install "Buku" "nixpkgs.buku"
 nix_install "cmus" "nixpkgs.cmus"
 nix_install "hub" "nixpkgs.gitAndTools.hub"
 nix_install "Icdiff" "nixpkgs.icdiff"
