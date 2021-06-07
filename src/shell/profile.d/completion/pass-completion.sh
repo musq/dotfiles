2470680139622ae433b959afdbfce3ac6571818a
Ashish Ranjan <ashish@tug.ro>
2021-06-07 14:44:50 +0530

    Modified pass-completion for MacOS

--- src/shell/profile.d/completion/pass-completion.sh
+++ src/shell/profile.d/completion/pass-completion.sh
@@ -5,20 +5,12 @@
 # This file is licensed under the GPLv2+. Please see COPYING for more information.
 
 _pass_complete_entries () {
-	local prefix="${PASSWORD_STORE_DIR:-$HOME/.password-store/}"
-	prefix="${prefix%/}/"
-	local suffix=".gpg"
-	local autoexpand=${1:-0}
+	prefix="${PASSWORD_STORE_DIR:-$HOME/.password-store/}"
+	suffix=".gpg"
+	autoexpand=${1:-0}
 
 	local IFS=$'\n'
 	local items=($(compgen -f $prefix$cur))
-
-	# Remember the value of the first item, to see if it is a directory. If
-	# it is a directory, then don't add a space to the completion
-	local firstitem=""
-	# Use counter, can't use ${#items[@]} as we skip hidden directories
-	local i=0 item
-
 	for item in ${items[@]}; do
 		[[ $item =~ /\.[^/]*$ ]] && continue
 
@@ -27,7 +19,7 @@ _pass_complete_entries () {
 		if [[ ${#items[@]} -eq 1 && $autoexpand -eq 1 ]]; then
 			while [[ -d $item ]]; do
 				local subitems=($(compgen -f "$item/"))
-				local filtereditems=( ) item2
+				local filtereditems=( )
 				for item2 in "${subitems[@]}"; do
 					[[ $item2 =~ /\.[^/]*$ ]] && continue
 					filtereditems+=( "$item2" )
@@ -45,22 +37,11 @@ _pass_complete_entries () {
 
 		item="${item%$suffix}"
 		COMPREPLY+=("${item#$prefix}")
-		if [[ $i -eq 0 ]]; then
-			firstitem=$item
-		fi
-		let i+=1
 	done
-
-	# The only time we want to add a space to the end is if there is only
-	# one match, and it is not a directory
-	if [[ $i -gt 1 || ( $i -eq 1 && -d $firstitem ) ]]; then
-		compopt -o nospace
-	fi
 }
 
 _pass_complete_folders () {
-	local prefix="${PASSWORD_STORE_DIR:-$HOME/.password-store/}"
-	prefix="${prefix%/}/"
+	prefix="${PASSWORD_STORE_DIR:-$HOME/.password-store/}"
 
 	local IFS=$'\n'
 	local items=($(compgen -d $prefix$cur))
@@ -71,12 +52,9 @@ _pass_complete_folders () {
 }
 
 _pass_complete_keys () {
-	local GPG="gpg"
-	which gpg2 &>/dev/null && GPG="gpg2"
-
 	local IFS=$'\n'
 	# Extract names and email addresses from gpg --list-keys
-	local keys="$($GPG --list-secret-keys --with-colons | cut -d : -f 10 | sort -u | sed '/^$/d')"
+	local keys="$(gpg2 --list-secret-keys --with-colons | cut -d : -f 10 | sort -u | sed '/^$/d')"
 	COMPREPLY+=($(compgen -W "${keys}" -- ${cur}))
 }
 
@@ -84,14 +62,13 @@ _pass()
 {
 	COMPREPLY=()
 	local cur="${COMP_WORDS[COMP_CWORD]}"
-	local commands="init ls find grep show insert generate edit rm mv cp git help version ${PASSWORD_STORE_EXTENSION_COMMANDS[*]}"
+	local commands="init ls find grep show insert generate edit rm mv cp git help version"
 	if [[ $COMP_CWORD -gt 1 ]]; then
 		local lastarg="${COMP_WORDS[$COMP_CWORD-1]}"
 		case "${COMP_WORDS[1]}" in
 			init)
 				if [[ $lastarg == "-p" || $lastarg == "--path" ]]; then
 					_pass_complete_folders
-					compopt -o nospace
 				else
 					COMPREPLY+=($(compgen -W "-p --path" -- ${cur}))
 					_pass_complete_keys
@@ -124,21 +101,10 @@ _pass()
 				COMPREPLY+=($(compgen -W "init push pull config log reflog rebase" -- ${cur}))
 				;;
 		esac
-
-		# To add completion for an extension command define a function like this:
-		# __password_store_extension_complete_<COMMAND>() {
-		#     COMPREPLY+=($(compgen -W "-o --option" -- ${cur}))
-		#     _pass_complete_entries 1
-		# }
-		#
-		# and add the command to the $PASSWORD_STORE_EXTENSION_COMMANDS array
-		if [[ " ${PASSWORD_STORE_EXTENSION_COMMANDS[*]} " == *" ${COMP_WORDS[1]} "* ]] && type "__password_store_extension_complete_${COMP_WORDS[1]}" &> /dev/null; then
-			"__password_store_extension_complete_${COMP_WORDS[1]}"
-		fi
 	else
 		COMPREPLY+=($(compgen -W "${commands}" -- ${cur}))
 		_pass_complete_entries 1
 	fi
 }
 
-complete -o filenames -F _pass pass
+complete -o filenames -o nospace -F _pass pass
