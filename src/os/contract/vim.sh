5d541f37d71ba6487010bdb1295615cfd81bd1ea
Ashish Ranjan <ashish@tug.ro>
2024-10-14 15:08:46 +0530

    Truncate repo for clean slate

--- src/os/contract/vim.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "${BASH_SOURCE[0]}")" \
-    && . "../utils.sh"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-nvim +PlugInstall +UpdateRemotePlugins +qa!
-print_result $? "Install vim plugins"
--- src/os/preferences/debian/main.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "${BASH_SOURCE[0]}")" \
-    && . "../../utils.sh"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-./privacy.sh
-./ui_and_ux.sh
