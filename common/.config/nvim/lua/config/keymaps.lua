6d4d04dc3af3ceb2d920d4b9b40f2dd9dccf7a6f
Ashish Ranjan <ashish@tug.ro>
2025-03-21 16:32:34 +0530

    alacritty, nvim: Use <Ctrl-I> to move forward in jumplist

--- common/.config/nvim/lua/config/keymaps.lua
+++ common/.config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,4 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<TAB>", "<CMD>FzfLua buffers sort_mru=true sort_lastused=true<CR>", { desc = "Pick buffers" })
+vim.keymap.set("n", "<C-n>i", "<C-i>", { noremap = true, desc = "Move forward in jumplist" })
