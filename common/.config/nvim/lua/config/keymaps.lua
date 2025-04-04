dc2efe930c47204074a91f5847e7cab41a35ce5b
Ashish Ranjan <ashish@tug.ro>
2025-04-04 13:35:45 +0530

    nvim: Add keymap <leader>zd to toggle window diff

--- common/.config/nvim/lua/config/keymaps.lua
+++ common/.config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,19 @@
 
 vim.keymap.set("n", "<TAB>", "<CMD>FzfLua buffers sort_mru=true sort_lastused=true<CR>", { desc = "Pick buffers" })
 vim.keymap.set("n", "<C-n>i", "<C-i>", { noremap = true, desc = "Move forward in jumplist" })
+
+-- Toggle Window Diff
+vim.keymap.set("n", "<leader>zd", function()
+  -- Get the current window diff status
+  local window_diff_status = vim.w.windowdiffstatus or "off"
+
+  if window_diff_status == "off" then
+    -- Enable diff mode in all windows
+    vim.cmd("windo diffthis")
+    vim.w.windowdiffstatus = "on"
+  else
+    -- Disable diff mode in all windows
+    vim.cmd("windo diffoff")
+    vim.w.windowdiffstatus = "off"
+  end
+end, { desc = "Toggle Window Diff" })
